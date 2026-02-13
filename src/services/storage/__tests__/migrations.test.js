e96a8b55365dacba37fa1c8b37fb45efe604ee2d
theshumba <ndoromelusi@gmail.com>
2026-02-13 02:38:19 +0000

    fix(31-01): update migration test for version 5

--- src/services/storage/__tests__/migrations.test.js
+++ src/services/storage/__tests__/migrations.test.js
@@ -41,8 +41,8 @@ describe('Storage Migrations', () => {
   });
 
   describe('Migration version', () => {
-    it('CURRENT_VERSION equals 1', () => {
-      expect(CURRENT_VERSION).toBe(1);
+    it('CURRENT_VERSION equals 5', () => {
+      expect(CURRENT_VERSION).toBe(5);
     });
 
     it('migrate is a function', () => {
